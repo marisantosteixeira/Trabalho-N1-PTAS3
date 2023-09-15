0831848c28d16dfe1540f6654768a56b5c6c585a
marisantosteixeira <mariana.teixeira2@estudante.ifms.edu.br>
2023-09-15 18:12:28 -0400

    config

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,13 @@ const { Sequelize } = require('sequelize');
 const config = require('../config/config')
 require('dotenv').config();
 
-const sequelize = new Sequelize(    );
+const sequelize = new Sequelize(config.development);
 
 try {
   sequelize.authenticate();
-  console.log('              ');
+  console.log('usuario autenticado ');
 } catch (error) {
-  console.error('             ', error);
+  console.error('usuario não autenticado ', error);
 }
 
 module.exports = { Sequelize, sequelize };
