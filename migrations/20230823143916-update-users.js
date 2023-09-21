c267bd84f5317e34fef49164c36bd717d3b7e9d5
marisantosteixeira <mariana.teixeira2@estudante.ifms.edu.br>
2023-09-21 18:20:40 -0400

    começou a aparecer um monte de erros no projeto do nada

--- migrations/20230823143916-update-users.js
+++ migrations/20230823143916-update-users.js
@@ -1,22 +1,10 @@
-'use strict';
-
-module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('users', 'createdAt', {
-      type: Sequelize.DATE,
-      allowNull: false,
-      defaultValue: Sequelize.literal('NOW()'),
-    });
-
-    await queryInterface.addColumn('users', 'updatedAt', {
-      type: Sequelize.DATE,
-      allowNull: false,
-      defaultValue: Sequelize.literal('NOW()'),
-    });
-  },
-
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn('users', 'createdAt');
-    await queryInterface.removeColumn('users', 'updatedAt');
-  },
-};
+te.</value>
+  </data>
+  <data name="Introduction10" type="System.String">
+    <value>View a list of all registered user accounts for the Web site.</value>
+  </data>
+  <data name="Introduction11" type="System.String">
+    <value>Change the authentication method that is used by the Web site. </value>
+  </data>
+  <data name="Introduction12" type="System.String">
+    <value>Note   You can create and manage user accounts, if you chose the &lt;span class=UI&gt;From the Internet&lt;/span&gt; option for your authentication type (if you are using forms-based authentication). If you chose the &lt;span class=UI&
