95b42e9897eac61a77a7956234376a482088f6a3
marisantosteixeira <mariana.teixeira2@estudante.ifms.edu.br>
2023-09-21 18:35:34 -0400

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,23 +4,23 @@ const jwt = require('jsonwebtoken');
 
 
 const createUser = async (req, res) => {
-    const {            } = req.body;
+    const { name, email, password } = req.body;
     await User.create({
-       
-
-
+        name: name,
+        email: email,
+        password: password
     }).then(() => {
-        res.json('         ');
-        console.log('           ');
+        res.json('Usuário cadastrado com sucesso');
+        console.log('Usuário cadastrado com sucesso');
     }).catch((erro) => {
-        res.json('             ');
-        console.log(`  : ${erro}`);
+        res.json(' Erro ao cadastrar');
+        console.log(`Erro ao cadastrar: ${erro}`);
     })
 }
 const findUsers = async (req, res) => {
-    const     = await User.findAll();
+    const users = await User.findAll();
     try {
-        res.json(    );
+        res.json(users);
     } catch (error) {
         res.status(404).json("Ocorreu um erro na busca!");
     };
@@ -31,57 +31,62 @@ const deleteUser = async (req, res) => {
     try {
         await User.destroy({
             where: {
-                
+                id: id
             }
         }).then(() => {
-            res.json("         ");
+            res.json("Usuário deletado com sucesso!");
         })
     } catch (error) {
-        res.status(404).json("      ");
+        res.status(404).json("Erro ao deletar usuário.");
     }
 }
+c
 const updateUser = async (req, res) => {
     const id = parseInt(req.params.id);
-    const {       } = req.body;
+    const { name, email, password } = req.body;
     try {
         await User.update(
             {
-               
+                name: name,
+                email: email,
+                password: password
             },
             {
                 where: {
                     id: id
                 }
             }
-        ).then(() => {
-            res.json("            ");
-        })
-    } catch (error) {
-        res.status(404).json("                !");
+            ).then(() => {
+                res.json("Usuário atualizado com sucesso!");
+            })
+        } catch (error) {
+            res.status(404).json("Erro ao atualizar usuário!");
+        }
     }
-}
-const authenticatedUser = async (req, res) => {
-    const {       } = req.body;
-    try {
-        const isUserAuthenticated = await User.findOne({
-            where: {
-                
-            }
-        })
-        const token = jwt.sign({
-            name: 
-            email: 
-        },
-            secret.secret, {
-            expiresIn: 86400,
-        })
-        return res.json({
-            name: 
-            email: isUserAuthenticated.email,
-            token: token
-        });
-    } catch (error) {
-        return res.json("");
+    
+    const authenticatedUser = async (req, res) => {
+        const { email, password } = req.body;
+        try {
+            const isUserAuthenticated = await User.findOne({
+                where: {
+                    email: email,
+                    password: password
+                }
+            })
+            const token = jwt.sign({
+                name: isUserAuthenticated.name,
+                email: isUserAuthenticated.email
+            },
+                secret.secret, {
+                expiresIn: 86400,
+            })
+            return res.json({
+                name: isUserAuthenticated.name,
+                email: isUserAuthenticated.email,
+                token: token
+            });
+        } catch (error) {
+        return res.json("Erro ao autenticar usuário");
     }
 }
 
